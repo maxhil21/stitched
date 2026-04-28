e3c1d60cabe0a9eb1cbd6b266f2b479bc9df5cc5
maxhil21 <max.hilden10@gmail.com>
2026-04-27 21:39:04 -0700

    Update tests/OutBlock.test.js
    Co-authored-by: A Murphy <murphdizle@Hotmail.com>

--- tests/OutBlock.test.js
+++ tests/OutBlock.test.js
@@ -16,10 +16,6 @@ jest.mock("react-native-reanimated", () =>
 
 jest.mock("react-native/src/private/animated/NativeAnimatedHelper.js");
 
-jest.mock("react-native-safe-area-context", () => ({
-    useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
-    SafeAreaView: ({ children }) => children,
-}));
 let mockOpenUrl = jest.fn();
 
 jest.mock("react-native", () => {
